49ed4ae64f6ed74cc7e744fa028568124605bdb2
onexixi <yangxixi0@qq.com>
2019-05-04 13:22:40 +0800

    add

---
+++ volatile/src/Volatile.java
@@ -0,0 +1,21 @@
+public class Volatile {
+
+//volatile线程可见性
+    public static void main(String[] args) {
+        MyDate date=new MyDate();
+        new Thread(()->{
+            date.getNumberAdd();
+            System.out.println("线程A"+Thread.currentThread().getName()+"Nmuber"+date.number);
+        },"A").start();
+
+        System.out.println(Thread.currentThread().getName()+"Nmuber"+date.number);
+    }
+
+}
+class MyDate{
+    volatile int number=0;
+
+    public  void getNumberAdd() {
+        this.number=60;
+    }
+}
