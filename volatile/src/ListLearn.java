33491b5a9f6d4b849da3f0cf8dc45833be7ffdae
onexixi <yangxixi0@qq.com>
2020-03-02 23:14:03 +0800

    add

--- volatile/src/ListLearn.java
+++ volatile/src/ListLearn.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ListLearn {
     public static void main(String[] args) {
@@ -14,7 +15,23 @@ public class ListLearn {
         HashSet set1=new LinkedHashSet();
         TreeSet treeSet=new TreeSet();
 
-        System.out.println( Collections.binarySearch(arrayList,"AA"));
+      //Z  System.out.println( Collections.binarySearch(arrayList,"AA"));
+
+        //既定顺序
+        List<String> sortStrings = Arrays.asList("香蕉", "苹果", "梨子", "芒果", "橙子");
+        //需要排序
+        List<String> needToSort = Arrays.asList("苹果", "香蕉", "苹果", "橙子", "芒果","梨子");
+        //通过对比 需要比较元素在ArrayList的index 就可以得到比较方法
+        //比for循环简洁
+        List<String> stringList=needToSort.stream().sorted(Comparator.comparingInt(sortStrings::indexOf)).collect(Collectors.toList());
+
+        sortStrings.forEach(System.out::print);
+        System.out.println("");
+        needToSort.forEach(System.out::print);
+        System.out.println("");
+        stringList.forEach(System.out::print);
+
+
 
 
     }
