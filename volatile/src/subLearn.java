1ac374fcd4fe8dd97b78acb84b57d20fb5987ee9
onexixi <yangxixi0@qq.com>
2020-03-29 20:00:04 +0800

    add

---
+++ volatile/src/subLearn.java
@@ -0,0 +1,24 @@
+import com.sun.javafx.binding.StringFormatter;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class subLearn {
+    public static void main(String[] args) {
+
+        String s="1.55555";
+        System.out.println (s);
+        String a= String.valueOf(StringFormatter.format("%.2s",s));
+        System.out.println (new Formatter().format("%.2f", Double.valueOf(s)).toString());
+
+        DecimalFormat formater = new DecimalFormat();
+        formater.setMaximumFractionDigits(2);
+        formater.setGroupingSize(0);
+        formater.setRoundingMode(RoundingMode.FLOOR);
+        System.out.println(formater.format(null));
+
+
+    }
+}
